31308a6700293835c7c91327de6067d01f81cc31
sejinjja <sejinjja@naver.com>
2023-03-18 21:32:40 +0900

    예제 코드 추가.

--- microTask.js
+++ microTask.js
@@ -5,16 +5,13 @@ function limiter () {
   count--
   return !count
 }
-
 function promiseTest () {
   return Promise.resolve().then(() => {
     makeElement(count)
     return !limiter() && promiseTest()
   })
 }
-
 promiseTest()
-
 function setTimoutTest () {
   setTimeout(() => {
     makeElement(count)
@@ -29,3 +26,28 @@ function makeElement (text) {
   console.log(text)
   document.body.appendChild(div)
 }
+
+for (let i = 0; i < 50000; i++) {
+  Promise.resolve.then(() => {
+    const div = document.createElement('div')
+    div.textContent = `Div ${i}`
+    document.body.appendChild(div)
+  }, 0)
+}
+
+///
+
+console.log('start')
+setTimeout(() => { console.log('setTimout') })
+
+const pr = Promise.resolve()
+
+pr.then(() => {
+  pr.then(() => console.log('promise then in promise then prev start'))
+  console.log('promise then start')
+  const cur = new Date()
+  while (new Date() - cur < 1000) {}
+  console.log('promise then end')
+  pr.then(() => console.log('promise then in promise then after end'))
+})
+console.log('end')
